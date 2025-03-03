e9084757dff4b27ec18fd3d617d9902e7e29d742
Ariel-250225 <seobin250225@icloud.com>
2025-03-03 13:02:02 +0700

    feature/project-build : 프로젝트 빌드
    프로젝트 빌드

---
+++ src/main/java/com/taekang/userservletapi/UserServletApiApplication.java
@@ -0,0 +1,13 @@
+package com.taekang.userservletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServletApiApplication.class, args);
+    }
+
+}
