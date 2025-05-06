c6bb731cd42fe3642773a9075c89a7d12fe9ee17
Ariel-250225 <seobin250225@icloud.com>
2025-05-06 10:16:34 +0700

    bugfix : memo, site nullish 적용

--- src/main/java/com/taekang/userservletapi/error/GlobalExceptionHandler.java
+++ src/main/java/com/taekang/userservletapi/error/GlobalExceptionHandler.java
@@ -1,19 +1,11 @@
 package com.taekang.userservletapi.error;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-  @ExceptionHandler(FailedMessageSendException.class)
-  public ResponseEntity<ErrorResponse> handleMessageSend(FailedMessageSendException e) {
-
-    return ResponseEntity.status(e.getErrorCode().getHttpStatus())
-        .body(new ErrorResponse(e.getErrorCode()));
-  }
 
   @ExceptionHandler(WalletVerification.class)
   public ResponseEntity<ErrorResponse> handleWalletVerification(WalletVerification e) {
@@ -47,10 +39,17 @@ public class GlobalExceptionHandler {
         .body(new ErrorResponse(e.getErrorCode()));
   }
 
-  // 다른 커스텀 예외들도 이렇게 추가하면 됨
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<ErrorResponse> handleOther(Exception ex) {
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
+  @ExceptionHandler(FailedMessageSendException.class)
+  public ResponseEntity<ErrorResponse> handleFailedMessageSendException(
+      FailedMessageSendException e) {
+    return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+        .body(new ErrorResponse(e.getErrorCode()));
   }
+
+//  // 다른 커스텀 예외들도 이렇게 추가하면 됨
+//  @ExceptionHandler(Exception.class)
+//  public ResponseEntity<ErrorResponse> handleOther(Exception ex) {
+//    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//        .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
+//  }
 }
