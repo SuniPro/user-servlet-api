746183a1536c2b95b47f0c6992f222fd91f5626d
Ariel-250225 <seobin250225@icloud.com>
2025-08-06 13:49:33 +0700

    feature : TokenResponse
    TokenResponse 구현

---
+++ src/main/java/com/taekang/userservletapi/DTO/TokenResponse.java
@@ -0,0 +1,13 @@
+package com.taekang.userservletapi.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private final String accessToken;
+    private final String refreshToken;
+    private final long accessTokenExpiresIn;
+    private final long refreshTokenExpiresIn;
+}
