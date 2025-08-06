0c5e29446aa2a5f3531a598740c50759d138ef9b
Ariel-250225 <seobin250225@icloud.com>
2025-08-06 13:42:21 +0700

    feature : User Service 추가
    유저 create만 우선적으로 구현

--- src/main/java/com/taekang/userservletapi/service/user/UserService.java
+++ src/main/java/com/taekang/userservletapi/service/user/UserService.java
@@ -1,15 +1,21 @@
 package com.taekang.userservletapi.service.user;
 
+import com.taekang.userservletapi.DTO.user.PasswordChangeDTO;
 import com.taekang.userservletapi.DTO.user.UserCreateRequestDTO;
-import com.taekang.userservletapi.entity.User;
+import com.taekang.userservletapi.DTO.user.UserDTO;
+import org.springframework.data.domain.Page;
 
 public interface UserService {
+  
+  UserDTO createUser(UserCreateRequestDTO userCreateRequestDTO);
 
-  User findById(Long userId);
+  UserDTO updateUser(Long userId, UserCreateRequestDTO request);
 
-  User createUser(UserCreateRequestDTO request);
-
-  void updateUser(Long userId, UserCreateRequestDTO request);
+  UserDTO passwordChange(UserDTO userDTO, PasswordChangeDTO passwordChangeDTO);
+  
+  Page<UserDTO> getAllUser();
+  
+  UserDTO getUserById(Long userId);
 
   void deleteUser(Long userId);
 }
