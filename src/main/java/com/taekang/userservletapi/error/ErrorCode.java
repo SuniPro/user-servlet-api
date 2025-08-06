3801fdb41748b162735b7e94e814bf167c02e698
Ariel-250225 <seobin250225@icloud.com>
2025-08-06 13:38:27 +0700

    refactor : TOKEN, USER 기능 추가에 따른 ERROR CODE 추가

--- src/main/java/com/taekang/userservletapi/error/ErrorCode.java
+++ src/main/java/com/taekang/userservletapi/error/ErrorCode.java
@@ -5,24 +5,31 @@ import org.springframework.http.HttpStatus;
 
 /**
  * 커스텀 예외에서 전달할 ErrorCode 리스트입니다. code의 prefix는 아래와 같이 정의합니다. C = Controller, S = Service 단에서, D =
- * Database에서 발생된 에러 C의 경우는 보통 rest API 소통 같의 문제, S의 경우 database 에서 가져온 데이터의 가공 처리 중 발생한 문제 D의 경우는
+ * Database에서 발생된 에러 C의 경우는 보통 rest API 소통 문제, S의 경우 database 에서 가져온 데이터의 가공 처리 중 발생한 문제 D의 경우는
  * 쓰레드 풀에서 요청한 데이터가 없을 경우로 정의된다고 판단하시면 됩니다. Code 의 숫자 나열은 HTTP 의 ERROR CODE에서 차용합니다. Code 만 받아도 이걸 본
  * 개발자는 바로 어떤 문제인지 식별되어야합니다.
  */
 @Getter
 public enum ErrorCode {
+  /* GLOBAL ERROR : 특정할 수 없는 에러 */
+  CANNOT_PROCESSING("C400", "처리할 수 없는 요청입니다.", HttpStatus.BAD_REQUEST),
   INTERNAL_SERVER_ERROR("S500", "서버 에러가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-  EMPLOYEE_NOT_FOUND("D404", "직원 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-  DUPLICATE_EMPLOYEE_NAME("C409", "중복된 이름입니다.", HttpStatus.CONFLICT),
-  PASSWORD_INCORRECT("C401", "잘못된 패스워드입니다.", HttpStatus.UNAUTHORIZED),
-  NAME_INCORRECT("C401", "잘못된 이름입니다.", HttpStatus.UNAUTHORIZED),
+
+  /* VALIDATION ERROR */
+  VALIDATION_FAILED("C400", "DTO 검증에 실패했습니다.", HttpStatus.BAD_REQUEST),
+
+  /* BOARD ERROR */
+  NOTIFY_NOT_FOUNDED("S404", "공지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
+  /* AUTH ERROR */
   TOKEN_EXPIRE("C401", "이미 만료된 토큰입니다.", HttpStatus.UNAUTHORIZED),
-  TOKEN_ABNORMALITY("C401", "이미 만료된 토큰입니다.", HttpStatus.UNAUTHORIZED),
   TOKEN_NOT_VALIDATE("C401", "인증되지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED),
   CANNOT_FIND_TOKEN("C401", "토큰을 찾을 수 없습니다.", HttpStatus.UNAUTHORIZED),
-  ABILITY_IS_EMPTY("S404", "해당 직원의 평가 기록이 없습니다.", HttpStatus.BAD_REQUEST),
-  NOTIFY_NOT_FOUNDED("S404", "공지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-  CANNOT_PROCESSING("C400", "처리할 수 없는 요청입니다.", HttpStatus.BAD_REQUEST),
+  IS_NOT_REFRESH_TOKEN("C400", "리프레시 토큰이 아닙니다.", HttpStatus.BAD_REQUEST),
+  IS_NOT_ACCESS_TOKEN("C400", "엑세스 토큰이 아닙니다.", HttpStatus.BAD_REQUEST),
+
+  /* FINANCIAL ERROR OF TETHER */
+  CANNOT_FOUNDED_EXCHANGE_INFO("S500", "환율정보 수령을 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
   ACCOUNT_NOT_FOUND("D404", "지갑정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   DUPLICATE_ACCOUNT("D409", "중복된 계좌입니다.", HttpStatus.CONFLICT),
   INVALID_AMOUNT("C400", "잘못된 금액입니다.", HttpStatus.BAD_REQUEST),
@@ -33,8 +40,13 @@ public enum ErrorCode {
   DUPLICATE_USERNAME_WITH_DIFFERENT_WALLET("S400", "잘못된 유저이름입니다.", HttpStatus.BAD_REQUEST),
   DEPOSIT_NOT_FOUND("D404", "입금 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   FAILED_MASSAGE_SEND("S500", "메일전송을 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-  CANNOT_FOUNDED_EXCHANGE_INFO("S500", "환율정보 수령을 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-  INVALID_EMAIL("C400", "잘못된 이메일입니다.", HttpStatus.BAD_REQUEST);
+
+  /* USER ERROR CODE */
+  INVALID_EMAIL("C400", "잘못된 이메일입니다.", HttpStatus.BAD_REQUEST),
+  CANNOT_FOUND_USER("D404", "유저 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+  ALREADY_EXIST_USERNAME("D400", "이미 존재하는 유저이름입니다.",  HttpStatus.BAD_REQUEST),
+  PASSWORD_INCORRECT("C401", "잘못된 패스워드입니다.", HttpStatus.UNAUTHORIZED),
+  ALREADY_EXIST_EMAIL("D400", "이미 존재하는 이메일입니다.",  HttpStatus.BAD_REQUEST);
 
   private final String code;
   private final String message;
