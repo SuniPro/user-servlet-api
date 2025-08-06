5ff0e36e1430ea37c8d0f0dac515c60dfaa2708a
Ariel-250225 <seobin250225@icloud.com>
2025-08-06 13:39:20 +0700

    refactor : ErrorResponse timestamp 추가
    이제 DTO 검증 실패 시 코드와 실패에 대한 상세 메세지를 같이 넘깁니다.

--- src/main/java/com/taekang/userservletapi/error/ErrorResponse.java
+++ src/main/java/com/taekang/userservletapi/error/ErrorResponse.java
@@ -1,21 +1,35 @@
 package com.taekang.userservletapi.error;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorResponse {
+
+  private String detailMessage;
   private String code;
   private String message;
-  private HttpStatus httpStatus;
+  private int httpStatus;
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  private final LocalDateTime timestamp = LocalDateTime.now();
 
   public ErrorResponse(ErrorCode errorCode) {
     this.code = errorCode.getCode();
     this.message = errorCode.getMessage();
-    this.httpStatus = errorCode.getHttpStatus();
+    this.httpStatus = errorCode.getHttpStatus().value();
+  }
+
+  public ErrorResponse(ErrorCode errorCode, String detailMessage) {
+    this.httpStatus  = errorCode.getHttpStatus().value();
+    this.code    = errorCode.getCode();
+    this.message = errorCode.getMessage();
+    this.detailMessage = detailMessage;
   }
 }
