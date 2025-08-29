8e2ac3fe4c1e2edc6375cdc6c1927394aa06455d
SuniPro <thtnsgus21@gmail.com>
2025-08-29 17:17:29 +0700

    feature : 입금 내역 검증을 받기 위한 DTO 추가

---
+++ src/main/java/com/taekang/userservletapi/DTO/crypto/CryptoTransferResponseDTO.java
@@ -0,0 +1,35 @@
+package com.taekang.userservletapi.DTO.crypto;
+
+import com.taekang.userservletapi.entity.user.ChainType;
+import com.taekang.userservletapi.entity.user.CryptoType;
+import com.taekang.userservletapi.entity.user.TransactionStatus;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CryptoTransferResponseDTO {
+
+    private ChainType chainType;
+
+    private CryptoType cryptoType;
+
+    private String fromAddress;
+
+    private String toAddress;
+
+    private String cryptoAmount;
+
+    private int decimals;
+
+    private TransactionStatus status;
+
+    private Boolean accepted;
+
+    private Long acceptedAt;
+    
+    private String memo;
+}
