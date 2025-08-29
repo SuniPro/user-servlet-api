cdd17b9e1cb2d120b51aa848e7cac15cb432f62f
SuniPro <thtnsgus21@gmail.com>
2025-08-29 17:38:04 +0700

    feature : rabbit MQ 구독을 통한 입금 승인 확인 시 이메일 발송 기능 추가

--- src/main/java/com/taekang/userservletapi/rabbitMQ/MessageConsumer.java
+++ src/main/java/com/taekang/userservletapi/rabbitMQ/MessageConsumer.java
@@ -2,6 +2,7 @@ package com.taekang.userservletapi.rabbitMQ;
 
 import com.taekang.userservletapi.DTO.crypto.CryptoDepositDTO;
 import com.taekang.userservletapi.service.MailSenderService;
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +26,8 @@ public class MessageConsumer {
     public void receiveDepositMessage(CryptoDepositDTO message) {
         log.info("Received deposit message: {}", message.toString());
 
-//        MimeMessage mail = mailSenderService.createDepositApprovalMail(message.getEmail(), message);
-//
-//        javaMailSender.send(mail);
-        // 여기서부터 받은 메시지(DTO)를 가지고 비즈니스 로직을 수행하면 됩니다.
-        // 예: employee-server에서 입금 승인 처리, 알림 발송 등
+        MimeMessage mail = mailSenderService.createDepositApprovalMail(message.getEmail(), message);
+
+        javaMailSender.send(mail);
     }
 }
