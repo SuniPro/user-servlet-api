7698b909a8128aac91c00fb9799f7144277cef19
SuniPro <thtnsgus21@gmail.com>
2025-08-29 17:18:35 +0700

    refactor : tether -> crypto

--- src/main/java/com/taekang/userservletapi/entity/user/CryptoWithdraw.java
+++ src/main/java/com/taekang/userservletapi/entity/user/CryptoWithdraw.java
@@ -1,15 +1,15 @@
-package com.taekang.userservletapi.entity;
+package com.taekang.userservletapi.entity.user;
 
 import jakarta.persistence.*;
 import lombok.*;
 
-@Table(name = "tether_withdraw")
+@Table(name = "crypto_withdraw")
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE) // 빌더 사용 시 필수
 @Builder
-public class TetherWithdraw extends TetherBaseTransaction {
+public class CryptoWithdraw extends CryptoBaseTransaction {
 
   @Id
   @Column(name = "id")
