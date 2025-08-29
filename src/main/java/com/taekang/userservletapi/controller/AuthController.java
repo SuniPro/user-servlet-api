3d03593c2133c656a5262c6a8c0129a36db67b3f
SuniPro <thtnsgus21@gmail.com>
2025-08-29 17:09:04 +0700

    feature : 토큰을 통한 유저 데이터 전송, refresh 엔드포인트 생성

---
+++ src/main/java/com/taekang/userservletapi/controller/AuthController.java
@@ -0,0 +1,98 @@
+package com.taekang.userservletapi.controller;
+
+import com.taekang.userservletapi.DTO.TokenResponse;
+import com.taekang.userservletapi.DTO.crypto.CryptoCreateDTO;
+import com.taekang.userservletapi.DTO.user.UserAccountInfo;
+import com.taekang.userservletapi.service.auth.AuthService;
+import com.taekang.userservletapi.util.auth.JwtUtil;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+  @Value("${server.servlet.context-path}")
+  private String contextPath;
+
+  private final AuthService authService;
+  private final JwtUtil jwtUtil;
+
+  @Autowired
+  public AuthController(AuthService authService, JwtUtil jwtUtil) {
+    this.authService = authService;
+    this.jwtUtil = jwtUtil;
+  }
+
+  @GetMapping("check")
+  public ResponseEntity<UserAccountInfo> check(@CookieValue("access-token") String token) {
+    String email = jwtUtil.getUserEmail(token);
+    String wallet = jwtUtil.getUserCryptoWallet(token);
+    String site = jwtUtil.getUserSite(token);
+    String chainType = jwtUtil.getUserChainType(token);
+    return ResponseEntity.ok(new UserAccountInfo(email, wallet, site, chainType));
+  }
+
+  @PostMapping("login")
+  public ResponseEntity<Void> login(@Valid @RequestBody CryptoCreateDTO cryptoCreateDTO) {
+
+    TokenResponse tokenResponse = authService.signIn(cryptoCreateDTO);
+
+    ResponseCookie accessCookie =
+        ResponseCookie.from("access-token", tokenResponse.getAccessToken())
+            .httpOnly(true)
+            .secure(true)
+            .path(contextPath)
+            .maxAge(tokenResponse.getAccessTokenExpiresIn())
+            .sameSite("Strict")
+            .build();
+
+    // Refresh Token Cookie
+    ResponseCookie refreshCookie =
+        ResponseCookie.from("refresh-token", tokenResponse.getRefreshToken())
+            .httpOnly(true)
+            .secure(true)
+            .path(contextPath)
+            .maxAge(tokenResponse.getRefreshTokenExpiresIn())
+            .sameSite("Strict")
+            .build();
+
+    return ResponseEntity.ok()
+        .header(HttpHeaders.SET_COOKIE, accessCookie.toString(), refreshCookie.toString())
+        .build();
+  }
+
+  @PostMapping("/refresh")
+  public ResponseEntity<Void> refresh(@CookieValue("refresh-token") String refreshToken) {
+    TokenResponse tokens = authService.refresh(refreshToken);
+
+    // 새 Access Token Cookie
+    ResponseCookie accessCookie =
+        ResponseCookie.from("access-token", tokens.getAccessToken())
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(tokens.getAccessTokenExpiresIn())
+            .sameSite("Strict")
+            .build();
+
+    // 새 Refresh Token Cookie
+    ResponseCookie refreshCookie =
+        ResponseCookie.from("refresh-token", tokens.getRefreshToken())
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(tokens.getRefreshTokenExpiresIn())
+            .sameSite("Strict")
+            .build();
+
+    return ResponseEntity.ok()
+        .header(HttpHeaders.SET_COOKIE, accessCookie.toString(), refreshCookie.toString())
+        .build();
+  }
+}
