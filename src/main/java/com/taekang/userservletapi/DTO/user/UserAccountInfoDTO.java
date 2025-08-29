06f5a7e4a324c6119014355e10272474c677c54b
SuniPro <thtnsgus21@gmail.com>
2025-08-29 17:35:34 +0700

    feature : 유저 정보 전달을 위한 DTO 추가

---
+++ src/main/java/com/taekang/userservletapi/DTO/user/UserAccountInfoDTO.java
@@ -0,0 +1,18 @@
+package com.taekang.userservletapi.DTO.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAccountInfoDTO {
+
+    private String email;
+    private String cryptoWallet;
+    private String site;
+
+//    getUserChainType과 통일성을 위해 String으로 하였음.
+    private String chainType;
+}
